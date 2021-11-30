c3a9d3b6daeff94a9aa126a1ee249c687b3d960d
evandangol <smartevu@gmail.com>
2021-11-30 10:26:34 +0545

    ew5rwe

--- sir/While.java
+++ sir/While.java
@@ -1,19 +1,29 @@
 package sir;
-public class While {
-    public static void main(String[] args) 
-    {
-            // int count=1; //initialization
-            //  while(count<=200) //condition
-            //  {
-            //       System.out.println("hello");
-            //       count=count+1;
-            //   }
 
-              int num=5;
-              while(num<7)
-              {
-                  System.out.println("hi");
-                  num=num+1;
-              }
+public class While {
+    public static void main(String[] args) {
+        // int count=1; //initialization
+        // while(count<=200) //condition
+        // {
+        // System.out.println("hello");
+        // count=count+1;
+        // }
+
+        // int num=5;
+        // while(num<7)
+        // {
+        // System.out.println("hi");
+        // num=num+1;
+        // }
+
+        // 7 table multiplication
+
+        int num = 9;
+        int count = 1;
+        while (count < 11) {
+            System.out.println(num + " X " + count + " = " + (num * count));
+            count = count + 1;
+        }
+
     }
 }
