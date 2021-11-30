df4a949afabf2eb95c7b03ebc0ab4b485a0b0096
evandangol <smartevu@gmail.com>
2021-11-30 10:08:51 +0545

    awedwa

--- While.java
+++ While.java
@@ -1,5 +1,18 @@
 public class While {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) 
+    {
+            // int count=1; //initialization
+            //  while(count<=200) //condition
+            //  {
+            //       System.out.println("hello");
+            //       count=count+1;
+            //   }
+
+              int num=5;
+              while(num<7)
+              {
+                  System.out.println("hi");
+                  num=num+1;
+              }
     }
 }
