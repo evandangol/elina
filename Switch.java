d2dc507dbec64d7996cad2d3cbb3a6bed4ed342d
evandangol <smartevu@gmail.com>
2021-11-30 09:33:12 +0545

    push

---
+++ Switch.java
@@ -0,0 +1,45 @@
+public class Switch
+ {
+    public static void main(String[] args) 
+    {
+        int month = 12;
+        switch (month) {
+        case 1:
+            System.out.println("January has " + 31 + " days");
+            break;
+        case 2:
+            System.out.println("February has " + 28 + " days");
+            break;
+        case 3:
+            System.out.println("March has " + 31 + " days");
+            break;
+        case 4:
+            System.out.println("April has " + 30 + " days");
+            break;
+        case 5:
+            System.out.println("May has " + 31 + " days");
+            break;
+        case 6:
+            System.out.println("June has " + 30 + " days");
+            break;
+        case 7:
+            System.out.println("july has " + 31 + " days");
+            break;
+        case 8:
+            System.out.println("August has " + 31 + " days");
+            break;
+        case 9:
+            System.out.println("Sepetember has " + 30 + " days");
+            break;
+        case 10:
+            System.out.println("October has " + 31 + " days");
+            break;
+        case 11:
+            System.out.println("November has " + 30 + " days");
+            break;
+        case 12:
+            System.out.println("December has " + 31 + " days");
+            break;
+        }
+    }
+}
