542f8439cf179b263bd7a985ab9d31b7267ac6de
evandangol <smartevu@gmail.com>
2021-11-30 11:29:55 +0545

    erter

--- elina/Evenodd.java
+++ elina/Evenodd.java
@@ -2,14 +2,16 @@ package elina;
 
 public class Evenodd {
     public static void main(String[] args) {
-        int count=1;
-        while(count<=20){ 
-            if(count%2==1)
+        int count = 1;
+        while (count <= 20) {
+            if (count % 2 == 1)
 
-            {System.out.println(count);}
-            count=count+1;
+            {
+                System.out.println(count);
+            }
+            count = count + 1;
 
         }
     }
-    
+
 }
--- elina/Product.java
+++ elina/Product.java
@@ -2,15 +2,15 @@ package elina;
 
 public class Product {
     public static void main(String[] args) {
-        int num=1;
-        int Prod=1;
-        while(num<6){
-            Prod=Prod* num;
-            num=num+1;
-
+        int num = 1;
+        int Prod = 1;
+        while (num < 6) {
+            Prod = Prod * num;
+            num = num + 1;
         }
         System.out.println(Prod);
 
     }
-    
+
 }
+https://us05web.zoom.us/j/86531417707?pwd=SjA2VHJ1RHJaY3Y1L3REQ3VzWHJFUT09
