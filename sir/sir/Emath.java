eaa04092008d86a55cda432727c99ff8af49b63e
evandangol <smartevu@gmail.com>
2021-12-11 11:18:03 +0545

    math functions

--- sir/sir/Emath.java
+++ sir/sir/Emath.java
@@ -3,7 +3,11 @@ public class Emath {
 public static void main(String[] args) {
     int a=10;
     int b=25;
- double max= Math.random();
- System.out.println(max);
+ int  randomnum=(int)( Math.random()* 100);
+ System.out.println(randomnum);
+//floor(x)
+
+System.out.println(Math.pow(5, 3));
+
 }
 }
