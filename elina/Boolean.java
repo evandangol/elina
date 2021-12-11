55f933c528bb6c30067af516fe3ac30ab09cd92a
Magarelina <magarelina784@gmail.com>
2021-12-11 14:21:12 +0800

    vhh

---
+++ elina/Boolean.java
@@ -0,0 +1,16 @@
+package elina;
+
+public class Boolean {
+    public static void main(String[] args) {
+        boolean exp = 4 > 7;
+        // System.out.println(exp);
+
+        // boolean exp1 = 4 > 2 || 8 < 9;
+        // System.out.println(exp1);
+
+        boolean exp2 = 4 < 3 && 8 < 10;
+        System.out.println(exp2);
+
+    }
+
+}
