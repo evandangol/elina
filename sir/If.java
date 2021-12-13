7db96c346ffb5d0446012393d5983f3c8e603d57
evandangol <smartevu@gmail.com>
2021-12-13 12:56:31 +0545

    sdfse

---
+++ sir/If.java
@@ -0,0 +1,69 @@
+public class If {
+    public static void main(String[] args) {
+        // if(8<9); //true
+
+        // String name="ram";
+
+        // if(name.equalsIgnoreCase("ram"));//true
+        // int x=5;
+        // if(x==5)//true
+
+
+
+        // if else if else------------------------------------------------------------
+        // int a = 20;
+
+        // if (a == 10)
+        // {
+        // System.out.println("if");
+        // }
+        // else
+        // {
+        // System.out.println("else");
+        // }
+
+        // int b = 30;
+
+        // if (b == 20)
+        // {
+        // System.out.println(20);
+        // }
+        // else if (b == 10)
+        // {
+        // System.out.println(10);
+        // }
+        // else if (b == 5)
+        // {
+        // System.out.println(5);
+        // }
+        // else
+        // {
+        // System.out.println("else");
+        // }
+
+        //even and odd---------------------------------------------------------------
+        // int x = 71;
+
+        // int rem = x % 2; // 0 1
+        // if (rem == 0) {
+        //     System.out.println("even");
+        // } else {
+        //     System.out.println("odd");
+        // }
+   
+    int num=0;
+    if(num>0)//true
+    {
+        System.out.println("positive");
+    }
+    else if(num<0)//true
+    {
+        System.out.println("negetive");
+    }
+    else{
+        System.out.println("Zero");
+    }
+
+
+    }
+}
---
+++ sir/Loop.java
@@ -0,0 +1,34 @@
+public class Loop {
+    public static void main(String[] args) {
+        //while for do
+
+
+        //while example 1
+        // int num=1;
+
+        // while(num<10)
+        // {
+        //     System.out.println(num);
+        //     num=num+1; //num++
+        // }
+
+
+         // print even number from 1 to 10
+
+       int num=1;
+        while (num<11)
+        {
+            int rem=num%2;
+            if(rem!=0)
+            {
+            System.out.println(num);
+            }
+            else
+            {
+
+            }
+            num++;
+        }
+
+    }
+}
