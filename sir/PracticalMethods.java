e708e62f8b4763bfbe5a9582ff84af079194340e
evandangol <smartevu@gmail.com>
2021-12-26 11:19:37 +0545

    dfesrfe

--- sir/PracticalMethods.java
+++ sir/PracticalMethods.java
@@ -1,7 +1,27 @@
 public class PracticalMethods {
     public static void main(String[] args) {
-        int num = 13;
-        IsPrime(num);
+       IsPalindrome(123);
+    }
+    // 123 =>321 not palindrome
+    // 121 =>121 palindrom
+
+    public static void IsPalindrome(int num) {
+        int back = num;
+        int reverse = 0;
+        int quo = 0;
+        
+        while (num != 0) {
+            int rem = num % 10;
+            reverse = reverse * 10 + rem;
+            quo = num / 10;
+            num = quo;
+        }
+        System.out.println(reverse);
+        if (reverse == back) {
+            System.out.println("palindrome");
+        } else {
+            System.out.println("not palindrome");
+        }
     }
 
     public static void IsPrime(int num) {
@@ -16,6 +36,25 @@ public class PracticalMethods {
             System.out.println("prime");
         } else {
             System.out.println("not prime");
+        }
+    }
+
+    public static boolean IsPrimeNum(int num) {
+        boolean prime = true;
+        for (int i = 2; i < num - 1; i++) {
+            if (num % i == 0) {
+                prime = false;
+                break;
+            }
+        }
+        return prime;
+    }
+
+    public static void PrintPrimeTo100() {
+        for (int i = 1; i <= 100; i++) {
+            if (IsPrimeNum(i) == true) {
+                System.out.print(i + ", ");
+            }
         }
     }
 
